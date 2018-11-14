9f868bc9e2739c87271994ff50de84bf33c20268
Vinicius Faria de Souza <vinicius.faria@tqi.com.br>
2018-11-14 11:06:27 -0200

    incluindo as tratativas do componente de mensagens junto ao servico e incluindo o servico ao servico de herois

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES)
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
